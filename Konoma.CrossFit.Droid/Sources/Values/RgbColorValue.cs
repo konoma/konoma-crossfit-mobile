b2faa58735d89e4360b9f76d4325380c79e9febd
Markus Gasser <markus.gasser@konoma.ch>
2020-06-02 22:00:13 +0200

    Add shareable argb colors

---
+++ Konoma.CrossFit.Droid/Sources/Values/RgbColorValue.cs
@@ -0,0 +1,17 @@
+﻿using Android.Graphics;
+using Android.Graphics.Drawables;
+
+namespace Konoma.CrossFit
+{
+    public static class RgbColorValueExtensions
+    {
+        public static Color ToNativeColor(this RgbColorValue color) => new Color(
+            r: color.Red,
+            g: color.Green,
+            b: color.Blue,
+            a: color.Alpha);
+
+        public static ColorDrawable ToNativeDrawable(this RgbColorValue color)
+            => new ColorDrawable(color.ToNativeColor());
+    }
+}
---
+++ Konoma.CrossFit/Sources/Values/RgbColorValue.cs
@@ -0,0 +1,47 @@
+﻿namespace Konoma.CrossFit
+{
+    /// <summary>
+    /// A platform agnostic way to represent an RGB color value with an alpha component.
+    /// </summary>
+    public struct RgbColorValue
+    {
+        /// <summary>
+        /// Creates a RgbColorValue from an integer in the ARGB format.
+        /// </summary>
+        /// <remarks>
+        /// If alpha is 0x00 this is interpreted as 0xFF instead. Use the
+        /// <see cref="RgbColorValue(byte, byte, byte, byte)"/> constructor
+        /// to create a color value with an explicit 0x00 alpha value.
+        /// </remarks>
+        /// <param name="argb">The argb value to parse.</param>
+        /// <returns></returns>
+        public static RgbColorValue FromArgb(int argb)
+        {
+            var a = (byte)(argb >> 24);
+            var r = (byte)(argb >> 16);
+            var g = (byte)(argb >> 8);
+            var b = (byte)(argb >> 0);
+
+            if (a == 0x00)
+            {
+                a = 0xFF;
+            }
+
+            return new RgbColorValue(red: r, green: g, blue: b, alpha: a);
+        }
+
+        public RgbColorValue(byte red, byte green, byte blue, byte alpha)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+            this.Alpha = alpha;
+        }
+
+        public byte Red { get; }
+        public byte Green { get; }
+        public byte Blue { get; }
+
+        public byte Alpha { get; }
+    }
+}
