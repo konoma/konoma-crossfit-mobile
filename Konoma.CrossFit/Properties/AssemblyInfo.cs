c63923d056b6257d2d7eb3b469809bb071042cd7
Markus Gasser <markus.gasser@konoma.ch>
2020-06-15 15:42:44 +0200

    Bump version number to 0.6.0

--- Konoma.CrossFit/Properties/AssemblyInfo.cs
+++ Konoma.CrossFit/Properties/AssemblyInfo.cs
@@ -1,4 +1,16 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Konoma.CrossFit.Droid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Konoma")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.6.0")]
 
 [assembly: InternalsVisibleTo("Konoma.CrossFit.iOS")]
 [assembly: InternalsVisibleTo("Konoma.CrossFit.Droid")]
