14dc5176ab441eb7f9357258f5eeb6bc5821cf34
Markus Gasser <markus.gasser@konoma.ch>
2020-06-15 15:57:11 +0200

    Remove incorrect assembly info

--- Konoma.CrossFit/Properties/AssemblyInfo.cs
+++ Konoma.CrossFit/Properties/AssemblyInfo.cs
@@ -1,16 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Konoma.CrossFit.Droid")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Konoma")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("0.6.0")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Konoma.CrossFit.iOS")]
 [assembly: InternalsVisibleTo("Konoma.CrossFit.Droid")]
