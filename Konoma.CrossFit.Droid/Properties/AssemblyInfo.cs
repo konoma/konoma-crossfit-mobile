e937b217b4c2181dcedaba6865c095fc6b58121e
Christoph Lipp <christoph.lipp@konoma.ch>
2020-09-21 08:36:06 +0200

    bump version to 0.7

--- Konoma.CrossFit.Droid/Properties/AssemblyInfo.cs
+++ Konoma.CrossFit.Droid/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
@@ -11,7 +11,7 @@
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.6.0")]
+[assembly: AssemblyVersion("0.7.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
